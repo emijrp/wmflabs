79ac794bf5b7791ac14f0f3e0f7cd2e5b221190f
Emilio J. Rodríguez-Posada <emijrp@gmail.com>
2014-05-16 20:53:42 +0200

    adding comments

--- queryalldb.py
+++ queryalldb.py
@@ -18,47 +18,59 @@
 import MySQLdb
 import sys
 
-query = "SELECT ss_total_edits FROM site_stats WHERE 1"
-if len(sys.argv) > 1:
-	query = sys.argv[1]
-
-conn = MySQLdb.connect(host='s3.labsdb', db='meta_p', read_default_file='~/replica.my.cnf', use_unicode=True)
-cursor = conn.cursor()
-cursor.execute("SELECT lang, family, slice, dbname FROM wiki WHERE 1;")
-result = cursor.fetchall()
-families = [
-    "wikibooks", 
-    "wikipedia", 
-    "wiktionary", 
-    "wikimedia", 
-    "wikiquote", 
-    "wikisource", 
-    "wikinews", 
-    "wikiversity", 
-    "commons", 
-    "wikispecies", 
-    "wikidata", 
-    "wikivoyage"
-    ]
-
-for row in result:
-    lang = row[0]
-    family = row[1]
-    if family not in families:
-        continue
-    dbserver = row[2]
-    dbname = row[3]+'_p'
+def main():
+    #This script receives a MySQL query as first parameter
+    #and runs it over all Wikimedia projects
+    #Modify it to do cool stuff with the data instead of print it on screen
+    #Remove or add wiki families as desired
     
-    try:
-        conn2 = MySQLdb.connect(host=dbserver, db=dbname, read_default_file='~/replica.my.cnf', use_unicode=True)
-        cursor2 = conn2.cursor()
-        cursor2.execute(query)
-        result2 = cursor2.fetchall()
-        for row2 in result2:
-            edits = int(row2[0])
-            print dbname, dbserver, edits
+    #default query or get query by parameter
+    query = "SELECT ss_total_edits FROM site_stats WHERE 1"
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+    
+    #get all wiki databases locations and names
+    conn = MySQLdb.connect(host='s3.labsdb', db='meta_p', read_default_file='~/replica.my.cnf', use_unicode=True)
+    cursor = conn.cursor()
+    cursor.execute("SELECT lang, family, slice, dbname FROM wiki WHERE 1;")
+    result = cursor.fetchall()
+    families = [
+        "wikibooks", 
+        "wikipedia", 
+        "wiktionary", 
+        "wikimedia", 
+        "wikiquote", 
+        "wikisource", 
+        "wikinews", 
+        "wikiversity", 
+        "commons", 
+        "wikispecies", 
+        "wikidata", 
+        "wikivoyage"
+        ]
+    
+    for row in result:
+        lang = row[0]
+        family = row[1]
+        if family not in families:
+            continue
+        dbserver = row[2]
+        dbname = row[3]+'_p'
+        
+        #run the same query over every database
+        try:
+            conn2 = MySQLdb.connect(host=dbserver, db=dbname, read_default_file='~/replica.my.cnf', use_unicode=True)
+            cursor2 = conn2.cursor()
+            cursor2.execute(query)
+            result2 = cursor2.fetchall()
+            for row2 in result2:
+                edits = int(row2[0])
+                print dbname, dbserver, edits
 
-        cursor2.close()
-        conn2.close()
-    except:
-        print "Error in", dbserver, dbname
+            cursor2.close()
+            conn2.close()
+        except:
+            print "Error in", dbserver, dbname
+
+if __name__ == '__main__':
+    main()
